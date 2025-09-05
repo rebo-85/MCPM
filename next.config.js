f60199723ecc81b78da4f1f5f3fc2b7be8277f87
ReBo <restrainboy@gmail.com>
2025-09-05 15:11:41 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const repo = "MCPM"; // your repo name
+module.exports = {
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+};
